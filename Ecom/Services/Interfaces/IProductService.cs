413f4e0efdd8726bf690635cbeb55d3534dca793
shubhasarkar53 <sarkarshubha123@gmail.com>
2026-08-22 11:09:39 +0530

    Feature - Adding Services - Added Services Interfaces.

---
+++ Ecom/Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Ecom.Models;
+
+namespace Ecom.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+        Task<bool> UpdateAsync(Product product);
+        Task<bool> DeleteAsync(int id);
+    }
+}
