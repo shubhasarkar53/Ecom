533194158a6e4ec1ae738b17c516786dedb8368c
shubhasarkar53 <sarkarshubha123@gmail.com>
2026-09-05 22:29:40 +0530

    Add JWT authentication and product service extensions
    Introduce `AuthServiceJWTExtension` for JWT authentication setup and `ProductServiceExtension` for registering product services. Refactor `Program.cs` to utilize these new extension methods, improving service registration and cleaning up unused namespaces.

---
+++ Ecom/ExtensionMethod/ProductServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Ecom.Models;
+using Ecom.Services;
+using Ecom.Services.Interfaces;
+
+namespace Ecom.ExtensionMethod
+{
+    public static class ProductServiceExtension
+    {
+        public static IServiceCollection AddProductServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductItemService, ProductItemService>();
+
+            return services;
+        }
+    }
+}
--- Ecom/Program.cs
+++ Ecom/Program.cs
@@ -1,16 +1,12 @@
 using Ecom.Data;
 using Ecom.Exceptions;
+using Ecom.ExtensionMethod;
 using Ecom.Services;
 using Ecom.Services.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 //controllers
 builder.Services.AddControllers();
@@ -22,29 +18,12 @@ builder.Services.AddProblemDetails();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //services
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductItemService, ProductItemService>();
+builder.Services.AddProductServices();
 
 //auth service
 builder.Services.AddScoped<IAuthService,AuthService>();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var jwt = builder.Configuration["Jwt:Key"];
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt!)),
-            ValidateLifetime = true,
-            ValidateIssuer = false,
-            ValidateAudience = false,
-
-            ClockSkew = TimeSpan.Zero
-
-        };
-    });
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 builder.Services.AddAuthorization();
 
